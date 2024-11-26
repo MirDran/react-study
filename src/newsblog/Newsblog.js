bb6361a563c8c876dba9ba50ff530f3d518824ae
JunHyun <bjsd2001a@naver.com>
2024-11-26 16:03:21 +0900

    반복처리

--- src/newsblog/Newsblog.js
+++ src/newsblog/Newsblog.js
@@ -14,12 +14,11 @@ function NewsBlog() {
     let [news3, setNews3] = useState('내일의 뉴스');
     let [news, setNews] = useState(['오늘의 뉴스', '어제의 뉴스', '내일의 뉴스']);
 
-    let [cnt1, setCnt1] = useState(0);
-    let [cnt2, setCnt2] = useState(0);
-    let [cnt3, setCnt3] = useState(0);
+    let [cnt1, setCnt1] = useState([0, 0, 0]);
+    let index = 0;
 
     let [modalFlag, setModalFlag] = useState(false);
-    // let [chn, setChn] = useState(['Today News','Yesterday News','Tomorrow News']);
+    let [chn, setChn] = useState(['Today News','Yesterday News','Tomorrow News']);
 
     return (
         <>
@@ -28,8 +27,54 @@ function NewsBlog() {
                 <div style={{ color: 'orange', fontSize: "20px" }}>{title}</div>
             </div>
 
+            {
+                news.map((item, index) => {
+                    return (
+                        <div className="post-list">
+                            <h4>
+                                <span onClick={() => {
+                                    setModalFlag(!modalFlag);
+                                }}>{news[index]}
+                                </span> 
+                                <span onClick={() => {
+                                    let temp = [...cnt1];
+                                    temp[index]++;
+                                    setCnt1(temp);
+                                }}> ♥ </span>
+                                {cnt1[index]} </h4>
+                            <p>내용 무</p>
+                            
 
-            <div className="post-list">
+                        </div>
+
+                    )
+                })
+                
+            }
+
+            {
+                modalFlag == true ? <Modal /> : null
+            }
+
+            <button onClick={() => {
+                // setNews(['Today News','어제의 뉴스','내일의 뉴스']); 주소가 바뀌면 재렌더링
+                //arry 새로운 배열 deep copy
+
+                // let temp = [...news]; //deep copy
+                // temp[0] = 'Today News'; // 0인덱스 값 변경
+                // temp[1] = 'Yesterday News'
+                // temp[2] = 'Tomorrow News'
+                // setNews(temp);  //news = temp;
+
+                // temp.shift(); // ['어제의 뉴스','내일의 뉴스'];
+                // temp.unshift('Today News'); // ['Today News','어제의 뉴스','내일의 뉴스'];
+
+                // temp.splice(0, 1, 'Today News'); // 0번째 배열에서 첫번 째 단어 삭제 후 'Today News' 추가
+
+            }}>제목 변경</button>
+            
+
+            {/* <div className="post-list">
                 <h4>
                     <span onClick={() => {
 
@@ -69,7 +114,7 @@ function NewsBlog() {
                     }}> ♥ </span> {cnt3} </h4>
                 <p>내용 무</p>
 
-            </div>
+            </div> */}
 
             {/* <div className="post-list">
             <h4>오늘의 뉴스</h4>
@@ -86,26 +131,7 @@ function NewsBlog() {
             <p>내용 무</p>
             </div> */}
 
-            {
-                modalFlag == true ? <Modal /> : null
-            }
-
-            <button onClick={() => {
-                // setNews(['Today News','어제의 뉴스','내일의 뉴스']); 주소가 바뀌면 재렌더링
-                //arry 새로운 배열 deep copy
-
-                let temp = [...news]; //deep copy
-                temp[0] = 'Today News'; // 0인덱스 값 변경
-                temp[1] = 'Yesterday News'
-                temp[2] = 'Tomorrow News'
-                setNews(temp);  //news = temp;
-
-                // temp.shift(); // ['어제의 뉴스','내일의 뉴스'];
-                // temp.unshift('Today News'); // ['Today News','어제의 뉴스','내일의 뉴스'];
-
-                // temp.splice(0, 1, 'Today News'); // 0번째 배열에서 첫번 째 단어 삭제 후 'Today News' 추가
-
-            }}>제목 변경</button>
+            
         </>
     );
 }
